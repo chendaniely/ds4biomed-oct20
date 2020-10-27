b23ef2c840085cdf227ba870c03d88846ce96366
Daniel Chen <chendaniely@gmail.com>
2020-10-26 20:33:12 -0400

    ggplot2 code update

--- 04-ggplot2.R
+++ 04-ggplot2.R
@@ -18,3 +18,16 @@ ggplot(data = blood) + geom_bar(mapping = aes(x = Recurrence))
 ggplot() + geom_bar(data = blood, mapping = aes(x = Recurrence))
 
 ggplot(data = blood) + geom_boxplot(aes(x = as.factor(TVol), y = Age))
+
+ggplot(blood) +
+  geom_violin(aes(x = as.factor(TVol), y = Age)) +
+  geom_jitter(aes(x = as.factor(TVol), y = Age))
+
+ggplot(blood) +
+  geom_point(aes(x = PVol, y = PreopPSA, color = as.factor(sGS))) +
+  facet_wrap(~ RBC.Age.Group)
+
+ggplot(blood) +
+  geom_point(aes(x = PVol, y = PreopPSA, color = as.factor(sGS))) +
+  facet_grid(Recurrence ~ RBC.Age.Group) +
+  theme_minimal()
